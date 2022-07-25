524d2939bedd95a7f30c6147c243b7b65ba3b6b9
JeongSoo Lee <jeongsoolee@korea.ac.kr>
2022-07-25 08:14:10 -0700

    remove sensitive files

--- custom-lisp/twitch-my-config.el
+++
@@ -1,8 +0,0 @@
-(setq twitch-api-username "namudontdie")
-
-(setq twitch-api-oauth-token
-      (auth-source-pick-first-password
-       :machine "twitch.tv"
-       :user "namudontdie"))
-
-(provide 'twitch-my-config)
