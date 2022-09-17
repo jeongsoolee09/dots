729e50fcec170db3764f78be5d05d6af77ce2bd5
이정수 <jslee@ijeongsus-iMac.attlocal.net>
2022-09-17 16:45:44 -0700

    update

---
+++ wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require "wezterm"
+-- local fennel = require "fennel"
+
+return {
+    font = wezterm.font 'D2Coding Ligature',
+    font_size = 18.0,
+    enable_tab_bar = false,
+    disable_default_mouse_bindings = true,
+    audible_bell = "Disabled",
+    window_close_confirmation = "NeverPrompt",
+}
