c11acdc4c6b7fb091c27a8937518ce07a183b94f
JeongSoo Lee <jeongsoolee@korea.ac.kr>
2022-09-18 09:46:48 -0700

    udpate

--- install-emacs-mac.sh
+++ install-emacs-mac.sh
@@ -1,1 +1,1 @@
-brew install emacs-mac --with-imagemagick --with-natural-title-bar --with-mac-metal --with-starter --with-emacs-big-sur-icon --with-xwidgets
+brew install emacs-mac --with-imagemagick --with-mac-metal --with-starter --with-emacs-big-sur-icon --with-xwidgets
