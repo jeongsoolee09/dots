0d6b7c9e42b5349406e23ac534377a91b97b2af6
이정수 <jslee@ijeongsus-iMac.attlocal.net>
2022-10-23 17:25:04 -0700

    update

--- init.el
+++ init.el
@@ -558,6 +558,24 @@
 
   (use-package kotlin-mode)
 
+  ;; scala config ====================================
+  ;; =================================================
+
+  (use-package scala-mode
+    :interpreter ("scala" . scala-mode))
+
+  (use-package sbt-mode
+    :commands sbt-start sbt-command
+    :config
+    ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+    ;; allows using SPACE when in the minibuffer
+    (substitute-key-definition
+     'minibuffer-complete-word
+     'self-insert-command
+     minibuffer-local-completion-map)
+    ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+    (setq sbt:program-options '("-Dsbt.supershell=false")))
+
   ;; git-gutter config ================================
   ;; ==================================================
 
